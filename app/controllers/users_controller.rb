9e293f5f88c023c3226af572df35184ab7018a31
Jeremy Walton <jthelegoman@gmail.com>
2014-03-10 14:07:47 -0400

    Addressed most comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
-	before_filter :authenticate_user!
-	def new
-
-	end
+  before_filter :authenticate_user!
+  def new
+  end
 
   def name
     render json: current_user
   end
-
 end
