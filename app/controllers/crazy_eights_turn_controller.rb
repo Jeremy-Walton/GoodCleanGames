ea992446c169ed90cf68b6909d137bbe6fb72b6b
Jeremy Walton <jthelegoman@gmail.com>
2014-03-26 11:46:15 -0400

    turns now work

--- app/controllers/crazy_eights_turn_controller.rb
+++ app/controllers/crazy_eights_turn_controller.rb
@@ -1,7 +1,10 @@
 class CrazyEightsTurnController < ApplicationController
+  before_filter :authenticate_for_api
+  skip_before_filter :verify_authenticity_token
 
   def create
-    @game = CrazyEightsGame.find_by(id: params[:id])
+    # binding.pry
+    @game = CrazyEightsGame.find(params[:crazy_eight_id])
     @game.data = params[:game]
     if @game.save
       render json: {game: {id: @game.id}}
